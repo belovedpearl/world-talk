a5f3c5d2124f962d7e9c30b44ed7bbe3d71d4cde
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-08-29 09:14:02 +0000

    Add accounts settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -44,12 +44,21 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'blog',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -62,6 +71,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'codestar.urls'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 TEMPLATES = [
     {
