b61b73470efe8d3a15bef011b315cf9b693678b9
Oluwaseun Omisakin <adedijioluwaseun@yahoo.com>
2023-08-30 03:58:21 +0000

    Setup the comments form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
@@ -22,7 +23,8 @@ class PostDetail(View):
             request, 'post_detail.html', {
                 'post': post,
                 'comments': comments,
-                'liked': liked
+                'liked': liked,
+                'comment_form': CommentForm()
             }
         )
 
